6253eacca7461f3ce658610e5da6dfa400b7d20f
dimahmz <essaadihamza4@outlook.com>
2024-10-24 19:22:46 +0100

    change the logger structure

--- src/logger.py
+++ src/logger.py
@@ -1,12 +1,31 @@
 import logging
 import os
 
-os.makedirs('src/logs/info', exist_ok=True)
-os.makedirs('src/logs/debug', exist_ok=True)
-os.makedirs('src/logs/errors', exist_ok=True)
-os.makedirs('src/logs/warnings', exist_ok=True)
-logging.basicConfig(filename='src/logs/info/index.log', level=logging.INFO)
-logging.basicConfig(filename='src/logs/debug/index.log', level=logging.DEBUG)
-logging.basicConfig(filename='src/logs/errors/index.log', level=logging.DEBUG)
-logging.basicConfig(filename='src/logs/warnings/index.log', level=logging.WARNING)
-logger = logging.getLogger(__name__)
+# Create the logs directory if it doesn't exist
+os.makedirs('src/logs', exist_ok=True)
+
+# Function to create a logger
+
+
+def setup_logger(name, log_file, level):
+    handler = logging.FileHandler(log_file)
+    handler.setLevel(level)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s - [%(pathname)s:%(lineno)d]')
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
+# Create separate loggers
+info_logger = setup_logger('info_logger', 'src/logs/info.log', logging.INFO)
+debug_logger = setup_logger(
+    'debug_logger', 'src/logs/debug.log', logging.DEBUG)
+error_logger = setup_logger(
+    'error_logger', 'src/logs/errors.log', logging.ERROR)
+warning_logger = setup_logger(
+    'warning_logger', 'src/logs/warnings.log', logging.WARNING)
